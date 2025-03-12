4c2e2ca16ca131674cb373dc80096b14a8832c65
LuisOrz <92197558+LuisOrz@users.noreply.github.com>
2025-03-12 00:23:57 -0600

    Update smilx_parameters.py

--- smilx_parameters.py
+++ smilx_parameters.py
@@ -14,8 +14,7 @@ class initial_parameters:
     st.markdown(
       """
       <div style="text-align: center;">
-        Centro de Investigación y Estudios Avanzados del Instituto Politécnico Nacional (CINVESTAV) <br>
-        THEOCHEM Mérida Yucatán <br>
+        Centro de Investigación y Estudios Avanzados del Instituto Politécnico Nacional (CINVESTAV) Mérida Yucatán<br>
         Luis Armando Gonzalez-Ortiz, Gabriel Merino, Amilcar Meneses, Lisset Noriega, and Filiberto Ortiz <br>
         GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 Copyright (C) 2007 Free Software Foundation <br>
           <br>
