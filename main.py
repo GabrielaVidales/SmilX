b3110cf9de6f438c02f94451ef9da917c3fad1cc
Gabriela Vidales <158981222+GabrielaVidales@users.noreply.github.com>
2025-03-24 11:41:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ st.markdown("""
             overflow: hidden;
             position: fixed;
             top: 0;
-            width: 100%;
-            z-index: 1000;
+            width: 120%;
+            z-index: 100;
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-            padding: 10px 0;
+            padding: 0;
         }
         
         .navbar-container {
