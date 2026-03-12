675510a7e72c5a6ffd0aef909e370595af3db03c
LuisOrz <92197558+LuisOrz@users.noreply.github.com>
2026-03-12 16:20:21 -0600

    Update smilx_chemical_space.py

--- smilx_chemical_space.py
+++ smilx_chemical_space.py
@@ -1221,14 +1221,14 @@ class chemical_space_carbenes:
           list_smiles_final.append(''.join(i_smiles.smiles))
     #------------------------------------------------------------------------------------ Section 8
     st.write(f"******************************Exploration completed: {len(list_smiles_final)} isomers found******************************")
+    with open(f"{parameters.filename_output_smi}", "r") as file:
+      st.download_button(
+          label="Download SMILES",
+          data=file,
+          file_name=f"{parameters.filename_output_smi}",
+          mime="text/smi",
+          )
     try:
-        with open(f"{parameters.filename_output_smi}", "r") as file:
-          st.download_button(
-              label="Download SMILES",
-              data=file,
-              file_name=f"{parameters.filename_output_smi}",
-              mime="text/smi",
-              )
         df = pd.DataFrame({"smi": list_smiles_final, "id": range(1, len(list_smiles_final) + 1)})
         mg = mols2grid.display(df, smiles_col="smi", subset=["id", "img", "smi"], n_cols=6, size = (130, 90))        
         html_grid = mg.data
