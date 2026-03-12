c03bc53dc404e2195882d42263f9059070b4dea6
LuisOrz <92197558+LuisOrz@users.noreply.github.com>
2026-03-11 19:16:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -467,10 +467,7 @@ def main():
         with st.spinner("Please wait..."):
             _ = chemical_space_classic(parameters)
     except:
-        if self.molecular_formula == none:
-            st.error("Review the hydrogen deficiency index, HDI, in the molecular formula")
-        else
-            st.error(f"Error in molecular formula syntax: {parameters.str_molecular_formula}")
+        st.error(f"Error in molecular formula syntax: {parameters.str_molecular_formula}")
 
     st.markdown("""
 <div class="description-text" id="about">
